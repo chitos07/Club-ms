8a6396861ee43ce101a91e9ccc6d65043a22786b
chitos07 <Chitosspirit@gmail.com>
2022-06-02 15:26:01 +0200

    bring all things that's have  been changed from the main-repo

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\CurrencyController;
-use App\Http\Controllers\BrancheController;
+use App\Http\Controllers\BranchController;
 use App\Http\Controllers\ResourceController;
 use App\Http\Controllers\CourseCategoryController;
 use App\Http\Controllers\CancellationPolicyController;
@@ -15,6 +15,7 @@ use App\Http\Controllers\CourseController;
 use App\Http\Controllers\RoleController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ClientController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -34,7 +35,7 @@ use App\Http\Controllers\ClientController;
 //Route::get('documentation',function (){
 //    return view('vendor.l5-swagger.index');
 //});
-Route::post('/user/login', [LoginController::class,'login']);
+Route::post('/users/login', [LoginController::class, 'login']);
 
 
 /*
@@ -42,137 +43,151 @@ Route::post('/user/login', [LoginController::class,'login']);
  */
 
 
-
-
-Route::group(['middleware' => ['auth:api']],function (){
-
+Route::group(['middleware' => ['auth:api']], function () {
 
     /* User Routes */
-    Route::group(['middleware' => ['scopes:user.restore']],function (){
-        Route::get('/user/restore',[UserController::class,'RestoreAll']);
-        Route::get('/user/restore/{id}',[UserController::class,'RestoreDeletedItemWithId']);
+    Route::group(['middleware' => ['scopes:user.restore']], function () {
+        Route::get('/users/restore', [UserController::class, 'RestoreAll']);
+        Route::get('/users/restore/{id}', [UserController::class, 'RestoreDeletedItemWithId']);
     });
-
-      Route::resource('/user',UserController::class)
-          ->middleware('scopes:user.index,user.create,user.edit,user.show,user.delete');
+    Route::get('/users', [UserController::class, 'index'])->middleware('scopes:user.index');
+    Route::post('/users', [UserController::class, 'store'])->middleware('scopes:user.create');
+    Route::get('/users/{user}', [UserController::class, 'show'])->middleware('scopes:user.show');
+    Route::put('/users/{user}', [UserController::class, 'update'])->middleware('scopes:user.edit');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->middleware('scopes:user.delete');
     /* End Routes */
 
     /* role Routes */
-    Route::group(['middleware' => ['scopes:role.restore']],function (){
-        Route::get('/role/restore',[RoleController::class,'RestoreAll']);
-        Route::get('/role/restore/{id}',[RoleController::class,'RestoreDeletedItemWithId']);
+    Route::group(['middleware' => ['scopes:role.restore']], function () {
+        Route::get('/roles/restore', [RoleController::class, 'RestoreAll']);
+        Route::get('/roles/restore/{id}', [RoleController::class, 'RestoreDeletedItemWithId']);
     });
-
-    Route::resource('/role',RoleController::class)
-        ->middleware('scopes:role.index,role.create,role.edit,role.show,role.delete');
+    Route::get('/roles', [RoleController::class, 'index'])->middleware('scopes:role.index');
+    Route::post('/roles', [RoleController::class, 'store'])->middleware('scopes:role.create');
+    Route::get('/roles/{role}', [RoleController::class, 'show'])->middleware('scopes:role.show');
+    Route::put('/roles/{role}', [RoleController::class, 'update'])->middleware('scopes:role.edit');
+    Route::delete('/roles/{role}', [RoleController::class, 'destroy'])->middleware('scopes:role.delete');
     /* End role Routes */
 
     /* Currency Routes */
-
-    Route::group(['middleware' => ['scopes:currency.restore']],function (){
-        Route::get('/currency/restore',[CurrencyController::class,'RestoreAll']);
-        Route::get('/currency/restore/{id}',[CurrencyController::class,'RestoreDeletedItemWithId']);
+    Route::group(['middleware' => ['scopes:currency.restore']], function () {
+        Route::get('/currencies/restore', [CurrencyController::class, 'RestoreAll']);
+        Route::get('/currencies/restore/{id}', [CurrencyController::class, 'RestoreDeletedItemWithId']);
     });
-
-    Route::resource('/currency',CurrencyController::class)
-        ->middleware('scopes:currency.index,currency.create,currency.edit,currency.show,currency.delete');
+    Route::get('/currencies', [CurrencyController::class, 'index'])->middleware('scopes:currency.index');
+    Route::post('/currencies', [CurrencyController::class, 'store'])->middleware('scopes:currency.create');
+    Route::get('/currencies/{currency}', [CurrencyController::class, 'show'])->middleware('scopes:currency.show');
+    Route::put('/currencies/{currency}', [CurrencyController::class, 'update'])->middleware('scopes:currency.edit');
+    Route::delete('/currencies/{currency}', [CurrencyController::class, 'destroy'])->middleware('scopes:currency.delete');
     /* End Currency Routes */
 
     /* Branche Routes */
-    Route::group(['middleware' => ['scopes:branche.restore']],function (){
-        Route::get('/branche/restore',[BrancheController::class,'RestoreAll']);
-        Route::get('/branche/restore/{id}',[BrancheController::class,'RestoreDeletedItemWithId']);
+    Route::group(['middleware' => ['scopes:branche.restore']], function () {
+        Route::get('/branches/restore', [BranchController::class, 'RestoreAll']);
+        Route::get('/branches/restore/{id}', [BranchController::class, 'RestoreDeletedItemWithId']);
     });
-
-    Route::resource('/branche',BrancheController::class)->middleware('scopes:branche.index,branche.create,branche.edit,branche.show,branche.delete');
+    Route::get('/branches', [BranchController::class, 'index'])->middleware('scopes:branch.index');
+    Route::post('/branches', [BranchController::class, 'store'])->middleware('scopes:branch.create');
+    Route::get('/branches/{branch}', [BranchController::class, 'show'])->middleware('scopes:branch.show');
+    Route::put('/branches/{branch}', [BranchController::class, 'update'])->middleware('scopes:branch.edit');
+    Route::delete('/branches/{branch}', [BranchController::class, 'destroy'])->middleware('scopes:branch.delete');
     /* End Branche Routes */
 
     /* Resoure Routes */
-    Route::group(['middleware' => ['scopes:resource.restore']],function (){
-        Route::get('/resource/restore',[ResourceController::class,'RestoreAll']);
-        Route::get('/resource/restore/{id}',[ResourceController::class,'RestoreDeletedItemWithId']);
+    Route::group(['middleware' => ['scopes:resource.restore']], function () {
+        Route::get('/resources/restore', [ResourceController::class, 'RestoreAll']);
+        Route::get('/resources/restore/{id}', [ResourceController::class, 'RestoreDeletedItemWithId']);
     });
-    Route::resource('/resource',ResourceController::class)->middleware('scopes:resource.index,resource.create,resource.edit,resource.show,resource.delete');
+    Route::get('/resources', [ResourceController::class, 'index'])->middleware('scopes:resource.index');
+    Route::post('/resources', [ResourceController::class, 'store'])->middleware('scopes:resource.create');
+    Route::get('/resources/{resource}', [ResourceController::class, 'show'])->middleware('scopes:resource.show');
+    Route::put('/resources/{resource}', [ResourceController::class, 'update'])->middleware('scopes:resource.edit');
+    Route::delete('/resources/{resource}', [ResourceController::class, 'destroy'])->middleware('scopes:resource.delete');
     /* End Resoure Routes */
 
     /* Course-Cat Routes */
-    Route::group(['middleware' => ['scopes:courseCategory.restore']],function (){
-        Route::get('/coursecategory/restore',[CourseCategoryController::class,'RestoreAll']);
-        Route::get('/coursecategory/restore/{id}',[CourseCategoryController::class,'RestoreDeletedItemWithId']);
+    Route::group(['middleware' => ['scopes:courseCategory.restore']], function () {
+        Route::get('/coursecategories/restore', [CourseCategoryController::class, 'RestoreAll']);
+        Route::get('/coursecategories/restore/{id}', [CourseCategoryController::class, 'RestoreDeletedItemWithId']);
     });
-
-        Route::resource('/coursecategory',CourseCategoryController::class)->except('show','update','destroy')->middleware('scopes:courseCategory.index,courseCategory.create');
-        Route::get('/coursecategory/{course_category}',[CourseCategoryController::class,'show'])->middleware('scopes:courseCategory.show');
-        Route::put('/coursecategory/{course_category}',[CourseCategoryController::class,'update'])->middleware('scopes:courseCategory.edit');
-        Route::delete('/coursecategory/{course_category}',[CourseCategoryController::class,'destroy'])->middleware('scopes:courseCategory.delete');
-
+    Route::get('/coursecategories', [CourseCategoryController::class,'index'])->middleware('scopes:courseCategory.index');
+    Route::post('/coursecategories', [CourseCategoryController::class, 'store'])->middleware('scopes:courseCategory.create');
+    Route::get('/coursecategories/{course_category}', [CourseCategoryController::class, 'show'])->middleware('scopes:courseCategory.show');
+    Route::put('/coursecategories/{course_category}', [CourseCategoryController::class, 'update'])->middleware('scopes:courseCategory.edit');
+    Route::delete('/coursecategories/{course_category}', [CourseCategoryController::class, 'destroy'])->middleware('scopes:courseCategory.delete');
     /* End Course-Cat Routes */
 
     /* CancellationPolicy Routes */
-    Route::group(['middleware' => ['scopes:cancellationPolicy.restore']],function (){
-        Route::get('/cancellationpolicy/restore',[CancellationPolicyController::class,'RestoreAll']);
-        Route::get('/cancellationpolicy/restore/{id}',[CancellationPolicyController::class,'RestoreDeletedItemWithId']);
+    Route::group(['middleware' => ['scopes:cancellationPolicy.restore']], function () {
+        Route::get('/cancellationpolicies/restore', [CancellationPolicyController::class, 'RestoreAll']);
+        Route::get('/cancellationpolicies/restore/{id}', [CancellationPolicyController::class, 'RestoreDeletedItemWithId']);
     });
-
-    Route::resource('/cancellationpolicy', CancellationPolicyController::class)->except('show', 'update', 'destroy')->middleware('scopes:cancellationPolicy.index');
-    Route::get('/cancellationpolicy/{cancellation_policy}', [CancellationPolicyController::class, 'show'])->middleware('scopes:cancellationPolicy.show');
-    Route::put('/cancellationpolicy/{cancellation_policy}', [CancellationPolicyController::class, 'update'])->middleware('scopes:cancellationPolicy.edit');
-    Route::delete('/cancellationpolicy/{cancellation_policy}', [CancellationPolicyController::class, 'destroy'])->middleware('scopes:cancellationPolicy.delete');
+    Route::get('/cancellationpolicies', [CancellationPolicyController::class,'index'])->middleware('scopes:cancellationPolicy.index');
+    Route::post('/cancellationpolicies', [CancellationPolicyController::class, 'store'])->middleware('scopes:cancellationPolicy.create');
+    Route::get('/cancellationpolicies/{cancellation_policy}', [CancellationPolicyController::class, 'show'])->middleware('scopes:cancellationPolicy.show');
+    Route::put('/cancellationpolicies/{cancellation_policy}', [CancellationPolicyController::class, 'update'])->middleware('scopes:cancellationPolicy.edit');
+    Route::delete('/cancellationpolicies/{cancellation_policy}', [CancellationPolicyController::class, 'destroy'])->middleware('scopes:cancellationPolicy.delete');
     /* End CancellationPolicy Routes */
 
     /* courseTemplate Routes */
-    Route::group(['middleware' => ['scopes:courseTemplate.restore']],function (){
-        Route::get('/coursetemplate/restore',[CourseTemplateController::class,'RestoreAll']);
-        Route::get('/coursetemplate/restore/{id}',[CourseTemplateController::class,'RestoreDeletedItemWithId']);
+    Route::group(['middleware' => ['scopes:courseTemplate.restore']], function () {
+        Route::get('/coursetemplates/restore', [CourseTemplateController::class, 'RestoreAll']);
+        Route::get('/coursetemplates/restore/{id}', [CourseTemplateController::class, 'RestoreDeletedItemWithId']);
     });
-
-
-
-        Route::resource('/coursetemplate', CourseTemplateController::class)->except('show', 'update', 'destroy')->middleware('scopes:courseTemplate.index,courseTemplate.create');
-        Route::get('/coursetemplate/{course_template}', [CourseTemplateController::class, 'show'])->middleware('scopes:cancellationPolicy.show');
-        Route::put('/coursetemplate/{course_template}', [CourseTemplateController::class, 'update'])->middleware('scopes:cancellationPolicy.edit');
-        Route::delete('/coursetemplate/{course_template}', [CourseTemplateController::class, 'destroy'])->middleware('scopes:cancellationPolicy.delete');
-
-
+    Route::get('/coursetemplates', [CourseTemplateController::class, 'index'])->middleware('scopes:courseTemplate.index,courseTemplate.create');
+    Route::post('/coursetemplates/', [CourseTemplateController::class, 'store'])->middleware('scopes:courseTemplate.s');
+    Route::get('/coursetemplates/{course_template}', [CourseTemplateController::class, 'show'])->middleware('scopes:courseTemplate.create');
+    Route::put('/coursetemplates/{course_template}', [CourseTemplateController::class, 'update'])->middleware('scopes:courseTemplate.edit');
+    Route::delete('/coursetemplates/{course_template}', [CourseTemplateController::class, 'destroy'])->middleware('scopes:courseTemplate.delete');
     /* End courseTemplate Routes */
 
     /* courseElement Routes */
-    Route::group(['middleware' => ['scopes:courseElement.restore']],function (){
-        Route::get('/courseelement/restore',[CourseElementController::class,'RestoreAll']);
-        Route::get('/courseelement/restore/{id}',[CourseElementController::class,'RestoreDeletedItemWithId']);
+    Route::group(['middleware' => ['scopes:courseElement.restore']], function () {
+        Route::get('/courseelements/restore', [CourseElementController::class, 'RestoreAll']);
+        Route::get('/courseelements/restore/{id}', [CourseElementController::class, 'RestoreDeletedItemWithId']);
     });
-
-        Route::resource('/courseelement', CourseElementController::class)->except('show', 'update', 'destroy')->middleware('scopes:courseElement.index,courseElement.create');;
-        Route::get('/courseelement/{course_element}', [CourseElementController::class, 'show'])->middleware('scopes:courseElement.show');
-        Route::put('/courseelement/{course_element}', [CourseElementController::class, 'update'])->middleware('scopes:courseElement.edit');
-        Route::delete('/courseelement/{course_element}', [CourseElementController::class, 'destroy'])->middleware('scopes:courseElement.delete');
-
+    Route::get('/courseelements', [CourseElementController::class, 'index'])->middleware('scopes:courseElement.index');
+    Route::post('/courseelements', [CourseElementController::class, 'store'])->middleware('scopes:courseElement.create');
+    Route::get('/courseelements/{course_element}', [CourseElementController::class, 'show'])->middleware('scopes:courseElement.show');
+    Route::put('/courseelements/{course_element}', [CourseElementController::class, 'update'])->middleware('scopes:courseElement.edit');
+    Route::delete('/courseelements/{course_element}', [CourseElementController::class, 'destroy'])->middleware('scopes:courseElement.delete');
     /* End courseElement Routes */
 
     /* Staff Routes */
-    Route::group(['middleware' => ['scopes:staff.restore']],function (){
-        Route::get('/staff/restore',[StaffController::class,'RestoreAll']);
-        Route::get('/staff/restore/{id}',[StaffController::class,'RestoreDeletedItemWithId']);
+    Route::group(['middleware' => ['scopes:staff.restore']], function () {
+        Route::get('/staffs/restore', [StaffController::class, 'RestoreAll']);
+        Route::get('/staffs/restore/{id}', [StaffController::class, 'RestoreDeletedItemWithId']);
     });
-    Route::resource('/staff',StaffController::class)->middleware('scopes:staff.index,staff.create,staff.edit,staff.show,staff.delete');
+    Route::get('/staffs', [StaffController::class, 'index'])->middleware('scopes:staff.index');
+    Route::post('/staffs', [StaffController::class, 'store'])->middleware('scopes:staff.create');
+    Route::get('/staffs/{staff}', [StaffController::class, 'show'])->middleware('scopes:staff.show');
+    Route::put('/staffs/{staff}', [StaffController::class, 'update'])->middleware('scopes:staff.edit');
+    Route::delete('/staffs/{staff}', [StaffController::class, 'destroy'])->middleware('scopes:staff.delete');
     /* End Staff Routes */
 
     /* Course Routes */
-    Route::group(['middleware' => ['scopes:course.restore']],function (){
-        Route::get('/course/restore',[CourseController::class,'RestoreAll']);
-        Route::get('/course/restore/{id}',[CourseController::class,'RestoreDeletedItemWithId']);
+    Route::group(['middleware' => ['scopes:course.restore']], function () {
+        Route::get('/courses/restore', [CourseController::class, 'RestoreAll']);
+        Route::get('/courses/restore/{id}', [CourseController::class, 'RestoreDeletedItemWithId']);
     });
-    Route::resource('/course',CourseController::class)->middleware('scopes:course.index,course.create,course.edit,course.show,course.delete');
+    Route::get('/courses', [CourseController::class, 'index'])->middleware('scopes:course.index');
+    Route::post('/courses', [CourseController::class, 'store'])->middleware('scopes:course.create');
+    Route::get('/courses/{course}', [CourseController::class, 'show'])->middleware('scopes:course.show');
+    Route::put('/courses/{course}', [CourseController::class, 'update'])->middleware('scopes:course.edit');
+    Route::delete('/courses/{course}', [CourseController::class, 'destroy'])->middleware('scopes:course.delete');
     /* End Course Routes */
-
 
     /* Course Routes */
-    Route::group(['middleware' => ['scopes:client.restore']],function (){
-        Route::get('/client/restore',[ClientController::class,'RestoreAll']);
-        Route::get('/client/restore/{id}',[ClientController::class,'RestoreDeletedItemWithId']);
+    Route::group(['middleware' => ['scopes:client.restore']], function () {
+        Route::get('/clients/restore', [ClientController::class, 'RestoreAll']);
+        Route::get('/clients/restore/{id}', [ClientController::class, 'RestoreDeletedItemWithId']);
     });
-    Route::resource('/client',ClientController::class)->middleware('scopes:client.index,client.create,client.edit,client.show,client.delete');
+    Route::get('/clients', [ClientController::class, 'index'])->middleware('scopes:client.index');
+    Route::post('/clients', [ClientController::class, 'store'])->middleware('scopes:client.create');
+    Route::get('/clients/{client}', [ClientController::class, 'show'])->middleware('scopes:client.show');
+    Route::put('/clients/{client}', [ClientController::class, 'update'])->middleware('scopes:client.edit');
+    Route::delete('/clients/{client}', [ClientController::class, 'destroy'])->middleware('scopes:client.delete');
     /* End Course Routes */
 
-});
 
+});
