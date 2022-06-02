eb5dedf138f8399c817a107212bbd38195c41aeb
chitos07 <Chitosspirit@gmail.com>
2022-06-02 15:16:12 +0200

    bring all things that's have  been changed from the main repo

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -18,13 +18,29 @@ class Role extends Model
     }
 
     public static function getroles(){
-
+	/*
         $getRoles = [];
 
         foreach (self::all() as $roles){
             $getRoles[$roles['role']]  =  str_replace('.',' can ',$roles->role);
         }
         return $getRoles;
+        */
+        
+        
+            /*
+         * This Condation to check if We have a Table call roles or not and that cuz we call this method in AuthServiceProvider
+         * And The AuthService Work Before a migration level so we must to do that to ِavoid no table error
+         */
+        if(!Schema::hasTable('roles')){
+          return [];
+        }
+
+        return self::all()->reduce(function($carry, $role) {
+            $carry[$role->role] = str_replace('.',' can ',$role->role);
+            return $carry;
+        }, []);
+
     }
 
 
